025c112207b0517ed3779605849bda3916a2a603
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-06-29 14:40:16 +0300

    add header and info section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import { Header } from './components/Header/Header';
+import { InfoSection } from './components/InfoSection/InfoSection';
 
 export const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <InfoSection />
     </div>
   );
 };
